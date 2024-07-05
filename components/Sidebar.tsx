5fc8a33d3ad49d0c2db7f8a09631292c28b9cd02
Henrique Pereira <henriquepermartins@gmail.com>
2024-07-05 11:27:46 -0300

    first homescreen

---
+++ components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sidebar = ({user}: SiderbarProps) => {
+  return (
+    <section className="sidebar">
+      <nav className='flex flex-col gap-4'>
+      </nav>
+    </section>
+  )
+}
+
+export default Sidebar
